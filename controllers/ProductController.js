9450d2f993f54312fb27ecb69ad4d49b2308ae74
kbastamow <bastamowkat@gmail.com>
2023-04-02 23:21:28 +0200

    Create product with multiple categories; getAll products with their categories

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,19 +2,40 @@ const { Product, Category } = require("../models/index.js");
 
 const ProductController = { 
     async create(req, res){
+       
+       /*I break the object sent through body apart so that I can extract categoryIds from postman.
+       They allow me to include more than one category on one go */
         try{
-            const product = await Product.create(req.body);
-            await product.addCategory(req.body.CategoryId);
+            const { name, price, description, image, categoryIds } = req.body;
+                      
+            const product = await Product.create({
+                name, 
+                price: price || null,  //default values if no value is provided (optional fields)
+                description: description || null,
+                image: image || null
+            });
+
+            for (const categoryId of categoryIds) {  //loop through categoryIds (array) to map product to multiple categories
+                await product.addCategory(categoryId);
+              }
             res.status(201).send({msg:"New product created", product }); 
         }catch(error){
             console.log(error);
             res.status(500).send(error);
         }
+    },
+
+    async getAllJoinCategories(req,res) {
+        try{
+            const products = await Product.findAll({
+                include: [{ model: Category, through: { attributes: [] } }],  //Adding categories to view
+              });
+            res.send(products);
+        }catch(error){
+            console.error(error);
+            res.status(500).send(error);
+        }
     }
-
-
- 
-
 }
 
 module.exports = ProductController;
--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 
 router.post("/createProduct", ProductController.create);
+router.get("/getAllJoinCategories", ProductController.getAllJoinCategories);
 
 module.exports = router;
